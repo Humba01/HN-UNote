3d79a0bd4c36accf49028d7b03055a1ed2da54dc
Humberto Bravo <humbandroid@gmail.com>
2021-02-03 10:31:28 -0300

    Adicionando Express.JS ao projeto

--- js/config/animation_unote.js
+++ js/config/animation_unote.js
@@ -1,67 +1,67 @@
-let slogan = document.getElementById("slogan");
+const slogan = document.getElementById("slogan");
 
 function animacaoSlogan() {
-  setTimeout(() => {
+  setTimeout(function() {
     slogan.style.borderLeftWidth = "3px";
     slogan.style.borderLeftStyle = "solid";
     slogan.style.borderLeftColor = "#0000ff";
-    setTimeout(() => {    
+    setTimeout(function() {    
       slogan.style.borderLeftWidth = "0px";
       slogan.style.borderLeftStyle = "solid";
       slogan.style.borderLeftColor = "#0000ff";
-      setTimeout(() => {
+      setTimeout(function() {
         slogan.style.borderRightWidth = "3px";
         slogan.style.borderRightStyle = "solid";
         slogan.style.borderRightColor = "#0000ff";
-        setTimeout(() => {
+        setTimeout(function() {
           slogan.style.borderRightWidth = "0px";
           slogan.style.borderRightStyle = "solid";
           slogan.style.borderRightColor = "#0000ff";
-          setTimeout(() => {
+          setTimeout(function() {
             slogan.style.borderLeftWidth = "3px";
             slogan.style.borderLeftStyle = "solid";
             slogan.style.borderLeftColor = "#00ff00";
-            setTimeout(() => {    
+            setTimeout(function() {    
               slogan.style.borderLeftWidth = "0px";
               slogan.style.borderLeftStyle = "solid";
               slogan.style.borderLeftColor = "#00ff00";
-              setTimeout(() => {
+              setTimeout(function() {
                 slogan.style.borderRightWidth = "3px";
                 slogan.style.borderRightStyle = "solid";
                 slogan.style.borderRightColor = "#00ff00";
-                setTimeout(() => {
+                setTimeout(function() {
                   slogan.style.borderRightWidth = "0px";
                   slogan.style.borderRightStyle = "solid";
                   slogan.style.borderRightColor = "#00ff00";
-                  setTimeout(() => {
+                  setTimeout(function() {
                     slogan.style.borderLeftWidth = "3px";
                     slogan.style.borderLeftStyle = "solid";
                     slogan.style.borderLeftColor = "#ffcc00";
-                    setTimeout(() => {    
+                    setTimeout(function() {    
                       slogan.style.borderLeftWidth = "0px";
                       slogan.style.borderLeftStyle = "solid";
                       slogan.style.borderLeftColor = "#ffcc00";
-                      setTimeout(() => {
+                      setTimeout(function() {
                         slogan.style.borderRightWidth = "3px";
                         slogan.style.borderRightStyle = "solid";
                         slogan.style.borderRightColor = "#ffcc00";
-                        setTimeout(() => {
+                        setTimeout(function() {
                           slogan.style.borderRightWidth = "0px";
                           slogan.style.borderRightStyle = "solid";
                           slogan.style.borderRightColor = "#ffcc00";
-                          setTimeout(() => {
+                          setTimeout(function() {
                             slogan.style.borderLeftWidth = "3px";
                             slogan.style.borderLeftStyle = "solid";
                             slogan.style.borderLeftColor = "#dddddd";
-                            setTimeout(() => {    
+                            setTimeout(function() {    
                               slogan.style.borderLeftWidth = "0px";
                               slogan.style.borderLeftStyle = "solid";
                               slogan.style.borderLeftColor = "#dddddd";
-                              setTimeout(() => {
+                              setTimeout(function() {
                                 slogan.style.borderRightWidth = "3px";
                                 slogan.style.borderRightStyle = "solid";
                                 slogan.style.borderRightColor = "#dddddd";
-                                setTimeout(() => {
+                                setTimeout(function() {
                                   slogan.style.borderRightWidth = "0px";
                                   slogan.style.borderRightStyle = "solid";
                                   slogan.style.borderRightColor = "#dddddd";
@@ -85,6 +85,6 @@ function animacaoSlogan() {
 
 animacaoSlogan();
 
-setInterval(() => {
+setInterval(function() {
   animacaoSlogan();
 }, 9620);
